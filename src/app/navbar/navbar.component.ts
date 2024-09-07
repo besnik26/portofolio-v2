8c1c598bb3af467300003fcddd79a16d9e69e972
besnik <besnikbeka2001@gmail.com>
2024-09-07 22:12:33 +0200

    Navbar update

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {Component, ElementRef, HostListener} from '@angular/core';
+import {NgClass} from "@angular/common";
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgClass, NgOptimizedImage],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  activeLang: any;
+  isDropdownOpen: boolean = false;
+  isSecondDropdownOpen:boolean = false;
+  isLangDropdownOpen:boolean = false;
+  isMenuOpen: boolean = false;
+  constructor(){
 
+  }
+
+  
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    this.isDropdownOpen = false;
+    this.isSecondDropdownOpen = false;
+    this.isLangDropdownOpen = false;
+  }
 }
