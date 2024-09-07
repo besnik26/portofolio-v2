a795f6c6043430b84749aa6f62ce1194c2c37b57
besnik <besnikbeka2001@gmail.com>
2024-09-07 16:51:51 +0200

    Minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,11 @@ import {NavbarComponent} from "./navbar/navbar.component";
 import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
 import {ProjectsComponent} from "./projects/projects.component";
-import {ContactComponent} from "./contact/contact.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HomeComponent, AboutComponent, ProjectsComponent, ContactComponent],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, AboutComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from '../about/about.component';
 import { ProjectsComponent } from '../projects/projects.component';
-import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AboutComponent, ProjectsComponent, ContactComponent],
+  imports: [AboutComponent, ProjectsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  downloadCV() {
+    const link = document.createElement('a');
+    link.href = 'assets/resume/Besnik-Beka-CV.pdf';  
+    link.download = 'Besnik-Beka-CV.pdf';  
+    link.click();
+  }
 }
